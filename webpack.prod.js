320501f9a51ad5d7b5af8ac2ad6f9502cc0b4632
Eddie Gomez <eddieegomez4@gmail.com>
2020-05-20 11:43:02 -0700

    bump

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,44 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 
 module.exports = merge(common, {
 
     mode: 'production',
     devtool: 'source-map',
+    performance: {
+        hints: 'warning'
+    },
+       optimization: {
+       namedModules: false,
+       namedChunks: false,
+       nodeEnv: 'production',
+       flagIncludedChunks: true,
+       occurrenceOrder: true,
+       concatenateModules: true,
+       splitChunks: {
+         hidePathInfo: true,
+         minSize: 30000,
+         maxAsyncRequests: 5,
+         maxInitialRequests: 3,
+       },
+       noEmitOnErrors: true,
+       checkWasmTypes: true,
+       minimize: true,
+     },
+    plugins: [
+        new CompressionPlugin({
+          filename: '[path].br[query]',
+          algorithm: 'brotliCompress',
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: {
+            // zlib’s `level` option matches Brotli’s `BROTLI_PARAM_QUALITY` option.
+            level: 11,
+          },
+          threshold: 10240,
+          minRatio: 0.8,
+          deleteOriginalAssets: false,
+        }),
+      ],
 });
